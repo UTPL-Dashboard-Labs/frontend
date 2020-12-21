53083ae6633ced40c699f774c59260b376720e74
davidPardoED <david.pardo@exmerdev.com>
2020-12-20 19:38:49 -0500

    DUL-15 Fis unused vars

--- src/components/login/login-form.js
+++ src/components/login/login-form.js
@@ -2,8 +2,13 @@ import React, { useState } from 'react';
 import './login-form.css';
 export default function LoginForm(){
     const [showError, setShoeError]= useState(false)
+
+    const Login = (e) => {
+        e.preventDefault()
+        setShoeError(!showError)
+    }
     return(
-        <form className='login-form'>
+        <form className='login-form' onSubmit={(e)=>{Login(e)}}>
             <input type='email' placeholder='Email'/>
             <input type='password' placeholder='Password'/>
             {showError && <p className='error-mesage'>*Error</p>}
